cc2aeb72c72d4e766859bd13f057af4e66fc781b
leeja <Leejaeyun95@naver.com>
2020-08-31 22:54:40 +0900

    view에서 URL을 바꾼다

--- asd/ch3/polls/views.py
+++ asd/ch3/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.http import Http404
 from django.http import HttpResponse
 
 from django.template import loader
@@ -15,8 +15,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
