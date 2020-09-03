98b08343e71669bc4843316216b553169e3350f1
leeja <Leejaeyun95@naver.com>
2020-09-04 00:55:33 +0900

    view.py 주석 추가

--- asd/ch3/polls/views.py
+++ asd/ch3/polls/views.py
@@ -22,6 +22,9 @@ def detail(request, question_id):
     # 두번째 인자 조건이 성립하지 않으면 Http404 익셉션 발생
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+    # render(request : 응답을 받아야 이 함수가 생성되고, 사용된다.
+    #        template_name : request와 함께 필수 파라미터. 화면에 내용을 뿌려야하니까 뿌릴 화면을 지정해줘야지?
+    #        context : 딕셔너리형,  디폴트는 빈 딕셔너리. 템플릿의 내용이 될 녀석이다)
 
 
 def results(request, question_id):
