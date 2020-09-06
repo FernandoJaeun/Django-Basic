89d4c958c3516c9026a3ba1860b5d437ab1fb09d
leeja <Leejaeyun95@naver.com>
2020-09-06 23:55:13 +0900

    books 모델 생성

--- asd/ch3/books/models.py
+++ asd/ch3/books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField('Author')  # 다대 다 관계처리
+    # 외래키가 바라보는 값이 삭제될 때 외래키 필드를 포함하는 모델 인스턴스(row)도 같이 삭제한다.
+    Publisher = models.ForeignKey('publisher', on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    salutation = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    website = models.URLField()
+
+    def __str__(self):
+        return
--- asd/ch3/mysite/settings.py
+++ asd/ch3/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.0.6']
 # Application definition
 
 INSTALLED_APPS = [
+    'books.apps.BooksConfig'
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
