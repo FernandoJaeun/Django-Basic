f8df6761f84e12421d4f6dbc089bab13863cc262
leejaeyun95 <leejaeyun95@naver.com>
2020-09-09 23:54:21 +0900

    Home화면 제작

--- asd/ch3/mysite/urls.py
+++ asd/ch3/mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
+
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('books/', include('books.urls')),
-    path('admin/', admin.site.urls),
 ]
